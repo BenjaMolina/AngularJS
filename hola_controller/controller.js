e0e468565d5d54caa3f726ae2dfeb37c619d49b8
BenjaMolina <pain.gedomazo.tensei@gmail.com>
2017-06-24 16:37:04 -0500

    Mi primer controlador

---
+++ hola_controller/controller.js
@@ -0,0 +1,22 @@
+var app = angular.module("MiPrimerApp",[])
+app.controller("FirsController",function($scope){
+	$scope.nombre = "Benjamin Molina";
+	/*$scope.nuevoComentario = {};
+	$scope.comentarios = 
+	[
+		{
+			comentario: "Buen Tutorail",
+			username: "CodigoFacilito"
+
+		},
+		{
+			comentario: "Malisimo Tutorail",
+			username: "otro_usuario"
+		}
+	];
+
+	$scope.agregarComentario = function(){
+		$scope.comentarios.push($scope.nuevoComentario);
+		$scope.nuevoComentario = {};
+	}*/
+});
