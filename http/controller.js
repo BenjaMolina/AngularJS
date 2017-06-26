efec5976d11f90c8b53e9d393233594f876076fc
BenjaMolina <pain.gedomazo.tensei@gmail.com>
2017-06-26 00:10:57 -0500

    AJAX con  2/2 (POST)

--- http/controller.js
+++ http/controller.js
@@ -1,6 +1,8 @@
 angular.module("MiPrimerApp",[]) // El segundo parametro hace referencia a los demas modulos que este depende 
 .controller("FirsController",function($scope,$http){
 	$scope.posts = [];
+	$scope.newPost = {};
+
 	$http.get("https://jsonplaceholder.typicode.com/posts")
 		.success(function(data){
 			console.log(data);
@@ -9,4 +11,20 @@ angular.module("MiPrimerApp",[]) // El segundo parametro hace referencia a los d
 		.error(function(err){
 
 		});
+
+	$scope.addPost = function(){
+		$http.post("https://jsonplaceholder.typicode.com/posts",{
+			title: $scope.newPost.title,
+			body: $scope.newPost.body,
+			userId: 1
+		})
+		.success(function(data,status,headers,config){
+			console.log(data);
+			$scope.posts.push(data);
+			$scope.newPost = {};
+		})
+		.error(function(error,status,headers,config){
+			console.log(error);
+		});
+	}
 });
